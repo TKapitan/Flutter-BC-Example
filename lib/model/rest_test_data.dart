ea69dd906d45159a3e2d2f1b8ed17bc305c8b932
Tomáš Kapitán <tkapitan@artex-is.cz>
2020-10-12 22:25:18 +0200

    Extend ListItem with Slidable functionality

--- lib/model/rest_test_data.dart
+++ lib/model/rest_test_data.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:tka_demo/page/components/list_item.dart';
 import 'package:tka_demo/programability/rest/rest_data.dart';
 
@@ -27,6 +29,13 @@ class RESTTestData extends RestData {
     return ListItem(
       title: '#' + this.postId.toString() + ' (' + this.email + ')',
       subtitle: this.body,
+      secondaryActions: <Widget>[
+        IconSlideAction(
+          caption: 'Delete',
+          color: Colors.red,
+          icon: Icons.delete,
+        ),
+      ],
     );
   }
 
--- lib/page/components/list_item.dart
+++ lib/page/components/list_item.dart
@@ -1,25 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class ListItem extends StatelessWidget {
   final String title, subtitle;
+  final List<Widget> actions, secondaryActions;
 
   ListItem({
     @required this.title,
     @required this.subtitle,
+    this.actions,
+    this.secondaryActions,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: ListTile(
-        title: Text(
-          this.title,
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
+    return Slidable(
+      enabled: (actions != null || secondaryActions != null),
+      actionPane: SlidableDrawerActionPane(),
+      actionExtentRatio: 0.25,
+      child: Container(
+        color: Colors.white,
+        child: Card(
+          child: ListTile(
+            title: Text(
+              this.title,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Text(this.subtitle),
           ),
         ),
-        subtitle: Text(this.subtitle),
       ),
+      actions: actions,
+      secondaryActions: secondaryActions,
     );
   }
 }
